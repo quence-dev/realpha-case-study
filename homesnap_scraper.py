f24ac16493e8a1f960f3bff68b3362574c13d38f
Spencer Vilicic <quence@bu.edu>
2021-07-15 19:14:37 -0400

    add search ability

--- homesnap_scraper.py
+++ homesnap_scraper.py
@@ -12,22 +12,21 @@ driver = webdriver.Chrome(PATH)
 driver.get('https://www.homesnap.com/') #open a webpage
 print(driver.title)
 
-searchbar = driver.find_element_by_tag_name('input') #find the HTML element
-searchbar.send_keys("Bowie, MD") #type a string into input
-searchbar.send_keys(Keys.RETURN) #hit enter
+main = driver.find_element_by_tag_name('main') #find the HTML element
+search = main.find_element_by_tag_name('input') 
+search.send_keys("Bowie, MD") #type a string into input
+search.send_keys(Keys.RETURN) #hit enter
 
-#print source code print(driver.page_source) 
-try:
-    results = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "0_0_2_9_8_divList"))
-    )
-    # print(results.text)
-
-    results_json = results.find_element_by_tag_name("script")
-    print(results_json.text)
-except:
-    print("Web driver failed")
-    driver.quit()
+# try:
+#     results = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located(By.ID, "0_0_2_9_8_divList")
+#     )
+#     # print(results.text)
+#     results_json = results.find_element_by_tag_name("script")
+#     print(results_json.text)
+# except:
+#     print("Web driver failed")
+#     driver.quit()
 
 time.sleep(5)
 
