dcba55aaef0a0dd5fd811ce815c0d5765f9053e8
Spencer Vilicic <quence@bu.edu>
2021-07-15 20:29:07 -0400

    incorporating beautiful soup

--- homesnap_scraper.py
+++ homesnap_scraper.py
@@ -5,29 +5,33 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from bs4 import BeautifulSoup as BS
+import requests
+import json
+import urllib3
 
 PATH = '/Users/Quence/Webdrivers/chromedriver' #wherever the webdriver is located
 driver = webdriver.Chrome(PATH)
 
 driver.get('https://www.homesnap.com/') #open a webpage
-print(driver.title)
+print("Connected to: "+driver.title)
 
 main = driver.find_element_by_tag_name('main') #find the HTML element
 search = main.find_element_by_tag_name('input') 
-search.send_keys("Bowie, MD") #type a string into input
+search.clear() #ensure text field is empty
+search.send_keys("New York, NY") #type a string into input
 search.send_keys(Keys.RETURN) #hit enter
 
-# try:
-#     results = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located(By.ID, "0_0_2_9_8_divList")
-#     )
-#     # print(results.text)
-#     results_json = results.find_element_by_tag_name("script")
-#     print(results_json.text)
-# except:
-#     print("Web driver failed")
-#     driver.quit()
+try:
+    # results = WebDriverWait(driver, 10).until(
+    #     EC.presence_of_element_located((By.TAG_NAME, 'script'))
+    # )
+    soup = BS(driver.current_url)
+    data = json.loads(soup.find('script', type='application/ld+json').text)
+    print(data)
+except:
+    print("JSON failed")
+    driver.quit()
 
 time.sleep(5)
-
 driver.quit()
