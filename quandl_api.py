350a0981aeade272567a1cb6e0180777be69ade2
Spencer Vilicic <quence@bu.edu>
2021-07-16 21:46:49 -0400

    use quandl module

--- quandl_api.py
+++ quandl_api.py
@@ -1,16 +1,11 @@
-import json
-import requests
 import quandl
+import json
+import datetime
 
 # load configs
 with open("config.json") as json_data_file:
     data = json.load(json_data_file)
-
 quandl.ApiConfig.api_key = data["api_key"]
-
-# key = data["api_key"]
-# endpoint = data["endpoint"]
-# response = requests.get(endpoint + 'indicator_id=ZSFH&region_id =99999&api_key=' + key)
 
 response = quandl.get_table('ZILLOW/DATA', indicator_id='ZSFH', region_id='99999')
 
