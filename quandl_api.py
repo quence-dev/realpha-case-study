57add3ce1f27e8417b54b0805a2f4b3873c887cf
Spencer Vilicic <quence@bu.edu>
2021-07-16 23:23:24 -0400

    add mysql support

--- quandl_api.py
+++ quandl_api.py
@@ -1,12 +1,30 @@
 import quandl
+import mysql.connector
 import json
 import datetime
 
 # load configs
 with open("config.json") as json_data_file:
     data = json.load(json_data_file)
-quandl.ApiConfig.api_key = data["api_key"]
 
-response = quandl.get_table('ZILLOW/DATA', indicator_id='ZSFH', region_id='99999')
+quandl.ApiConfig.api_key = data["api_key"]
+pw = data["mysql"]
+
+# connect to MySQL
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = pw,
+    database = "test"
+)
+
+cursor = db.cursor()
+# cursor.execute("CREATE DATABASE test")
+
+# key = data["api_key"]
+# endpoint = data["endpoint"]
+# response = requests.get(endpoint + 'indicator_id=ZSFH&region_id =99999&api_key=' + key)
+
+response = quandl.get_table('ZILLOW/DATA', indicator_id='ZSFH', region_id='10001')
 
 print(response)
