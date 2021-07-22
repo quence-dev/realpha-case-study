c848cd7900a968bfef4733d28d192724ddd92086
Spencer Vilicic <quence@bu.edu>
2021-07-21 21:24:29 -0400

    build table, limited to 5

--- homesnap_scraper.py
+++ homesnap_scraper.py
@@ -22,34 +22,28 @@ search.clear() #ensure text field is empty
 search.send_keys("New York, NY") #type a string into input
 search.send_keys(Keys.RETURN) #hit enter
 
-######## auto scroller #########
-# pause = 0.5
-# last_height = driver.execute_script("return document.body.scrollHeight")
-# print("Last height: %d" % last_height)
+def autoScroller():
+    ######## auto scroller #########
+    # pause = 0.5
+    # last_height = driver.execute_script("return document.body.scrollHeight")
+    # print("Last height: %d" % last_height)
 
-# while True:
-#     # Scroll down to bottom
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # while True:
+    #     # Scroll down to bottom
+    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-#     # Wait to load page
-#     time.sleep(pause)
+    #     # Wait to load page
+    #     time.sleep(pause)
 
-#     # Calculate new scroll height and compare with last scroll height
-#     new_height = driver.execute_script("return document.body.scrollHeight")
-#     if new_height == last_height:
-#         break
-#     last_height = new_height
-####################################
+    #     # Calculate new scroll height and compare with last scroll height
+    #     new_height = driver.execute_script("return document.body.scrollHeight")
+    #     if new_height == last_height:
+    #         break
+    #     last_height = new_height
+    ####################################
+    return None
 
 try:
-    # main2 = driver.find_element_by_tag_name('main')
-    # scripts = main2.find_element_by_xpath('//*[contains(@id,"divList")]/script')
-    # results = WebDriverWait(driver, 10).until(
-    #     EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'span[itemprop="streetAddress"]'))
-    # )
-    # results = main.find_element_by_tag_name('script')
-    # print(script.get_attribute('outerHTML'))
-
     time.sleep(15)
     
     results = WebDriverWait(driver, 10).until(
@@ -59,29 +53,31 @@ try:
     property_data = []
     for result in results:
         status = result.find_element_by_class_name('pa-item-status-bar').text
-        price = result.find_element_by_class_name('pa-item-paging-price').text
+        price = result.find_element_by_class_name('pa-item-paging-price').text.split('$')[1]
         address = result.find_element_by_css_selector('span[itemprop="streetAddress"]').text
-        city = result.find_element_by_css_selector('span[itemprop="addressLocality"]').text
+        city = result.find_element_by_css_selector('span[itemprop="addressLocality"]').text.split(',')[0]
         state = result.find_element_by_css_selector('span[itemprop="addressRegion"]').text
         zipcode = result.find_element_by_css_selector('span[itemprop="postalCode"]').text
 
-        # property_item = {
-        #     'status': status.text,
-        #     'price': price.text,
-        #     'address': address.text,
-        #     'city': city.text,
-        #     'state': state.text,
-        #     'zipcode': zipcode.text
-        # }
-        # property_data.append(property_item)
-        # print('item stored')
-    
-    # make dataframe to display list
-    # df = pd.DataFrame(property_data)
-    # print(df)
+        property_item = {
+            'status': status,
+            'price': price,
+            'address': address,
+            'city': city,
+            'state': state,
+            'zipcode': zipcode
+        }
+        property_data.append(property_item)
+    print('process complete')
 except:
     print("JSON failed")
     driver.quit()
+
+#  make dataframe to display list
+df = pd.DataFrame(property_data)
+# convert price to integer (https://stackoverflow.com/questions/39684548)
+df.price = df['price'].replace({'K': '*1e3', 'M': '*1e6'}, regex=True).apply(pd.eval).astype(int)
+print(df)
 
 time.sleep(5)
 driver.quit()
