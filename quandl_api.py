25072830a24dc14dbac70aaaf1a35f1dbd5e7907
Spencer Vilicic <quence@bu.edu>
2021-07-27 19:49:55 -0400

    experimenting with api vs module

--- quandl_api.py
+++ quandl_api.py
@@ -1,7 +1,9 @@
 import mysql.connector
 import json
+from pandas.core.frame import DataFrame
 import requests
 import numpy as np
+import pandas as pd
 from config import api_key, endpoint, mysql_pw
 import quandl
 # import pandas as pd
@@ -26,35 +28,39 @@ cursor = db.cursor()
 
 # run GET request on API
 def getData(indicator,region):
-    response = requests.get(endpoint + 'indicator_id=' + indicator + '&region_id=' + region + '&api_key=' + api_key)
+    response = requests.get(endpoint 
+    + 'indicator_id=' + indicator 
+    + '&region_id=' + region 
+    + '&api_key=' + api_key)
     return response.json()
 
 # prints each column in datatable
-# homes = getData('ZSFH', '10001')
+homes = getData('ZSFH', '10001')
 
 # save raw json file
 # with open('data.json', 'w') as f:
 #     json.dump(homes, f)
 
-# price_array = []
-# for home in homes['datatable']['data']:
-#     price_array.append(home[3])
-#     print(home)
+price_array = []
+for home in homes['datatable']['data']:
+    price_array.append(home[3]) # property price
 
-# np_price = np.array(price_array)
-# avg_price = np.average(np_price)
+# numpy calculations on price array
+np_price = np.array(price_array)
+avg_price = np.average(np_price)
 
-# print('Number of listings: %d' % np_price.size)
-# print(int(avg_price))
+print('Number of listings: %d' % np_price.size)
+print('Average price: %d' % int(avg_price))
 
 # DONE USING QUANDL MODULE:
 # quandl.ApiConfig.api_key = data["api_key"]
+# ZSFH, ZATT, Z2BR, Z3BR, Z4BR, Z5BR
 quandl.ApiConfig.api_key = api_key
-response = quandl.get_table('ZILLOW/DATA', indicator_id='ZSFH', region_id='10001')
-print(response)
+response = quandl.get_table('ZILLOW/DATA', indicator_id='ZSFH', region_id='90210')
+# print(response)
 
 response = quandl.get_table('ZILLOW/INDICATORS')
-print(response)
+# print(response)
 
 response = quandl.get_table('ZILLOW/REGIONS')
-print(response)
+# print(response)
