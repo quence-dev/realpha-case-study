d058ccfdbd12c267eb6ffcfd9e0652bed5e33016
Spencer Vilicic <quence@bu.edu>
2021-07-27 23:08:35 -0400

    database created

--- mysql_controller.py
+++ mysql_controller.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from config import mysql_pw
+import json
 
 # connect to MySQL
 db = mysql.connector.connect(
@@ -10,5 +11,16 @@ db = mysql.connector.connect(
 )
 cursor = db.cursor()
 
+with open("data.json") as json_data_file:
+    data = json.load(json_data_file)
+
 # cursor.execute("CREATE DATABASE allrecipes")
-# cursor.execute("CREATE TABLE recipes (id int PRIMARY KEY AUTO_INCREMENT, title VARCHAR(50), summary TEXT, rating FLOAT, rating_count INT, url TEXT, author VARCHAR(50))")
+# cursor.execute("CREATE TABLE recipes (id int PRIMARY KEY AUTO_INCREMENT, title TEXT, summary TEXT, rating FLOAT, rating_count INT, url TEXT, author VARCHAR(50))")
+# for items in data:
+#     cursor.execute("INSERT INTO recipes(title, summary, rating, rating_count, url, author) VALUES (%s, %s, %s, %s, %s, %s)", 
+#     (items['title'], items['summary'], items['rating'], items['rating_count'], items['url'], items['author']))
+
+cursor.execute('SELECT * FROM recipes')
+
+for x in cursor:
+    print(x)
