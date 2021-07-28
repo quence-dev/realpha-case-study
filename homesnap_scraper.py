34e20aae6fc9f5c23d9e9ea33fbe40ba34964818
Spencer Vilicic <quence@bu.edu>
2021-07-27 20:27:56 -0400

    loading pages not working

--- homesnap_scraper.py
+++ homesnap_scraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys #allows keystrokes
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 import json
 import pandas as pd
@@ -13,7 +14,7 @@ PATH = '/Users/Quence/Webdrivers/chromedriver'
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
 chrome_options.add_argument("--disable-notifications")
-driver = webdriver.Chrome(PATH, chrome_options=chrome_options)
+driver = webdriver.Chrome(PATH, options=chrome_options)
 
 # open a webpage
 driver.get('https://www.homesnap.com/')
@@ -25,30 +26,21 @@ search.clear() #ensure text field is empty
 search.send_keys("New York, NY") #type a string into input
 search.send_keys(Keys.RETURN) #hit enter
 
-def autoScroller():
-    ######## auto scroller #########
-    # pause = 0.5
-    # last_height = driver.execute_script("return document.body.scrollHeight")
-    # print("Last height: %d" % last_height)
-
-    # while True:
-    #     # Scroll down to bottom
-    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-    #     # Wait to load page
-    #     time.sleep(pause)
-
-    #     # Calculate new scroll height and compare with last scroll height
-    #     new_height = driver.execute_script("return document.body.scrollHeight")
-    #     if new_height == last_height:
-    #         break
-    #     last_height = new_height
-    ####################################
-    return None
+# load a few pages
+actions = ActionChains(driver)
+try:
+    load_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//*[contains(@id,'divAreaStats')]"))
+    )
+    for i in range(4):
+        actions.perform()
+        time.sleep(1)
+except:
+    print("can't find element")
+actions.move_to_element(load_element)
 
 try:
-    time.sleep(15)
-    
+    time.sleep(10)
     results = WebDriverWait(driver, 10).until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[class="pull-left ma-0 mb-5 ml-5 clearfix search-list-pa-item ps-r"]'))
     )
@@ -71,12 +63,25 @@ try:
             'zipcode': zipcode
         }
         property_data.append(property_item)
-        time.sleep(0.25)
 
     print('process complete')
 except:
     print("JSON failed")
     driver.quit()
+
+# doesn't work
+# market_stats = driver.find_element_by_class_name('col-xs-12 list-unstyled mb-15 ph-20')
+# market_stat = market_stats.find_elements_by_class_name('pull-right')
+# market_data = {
+#     'new_listings': market_stat[0].text,
+#     'homes': market_stat[1].text,
+#     'condos': market_stat[2].text,
+#     'detached': market_stat[3].text,
+#     'townhouse': market_stat[4].text,
+#     'median_list': market_stat[5].text,
+#     'median_sale': market_stat[6].text
+# }
+# print(market_data)
 
 #  make dataframe to display list
 df = pd.DataFrame(property_data)
