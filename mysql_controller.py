9592ee2e669f32799a4fff65aae61716f91f7ddc
Spencer Vilicic <quence@bu.edu>
2021-07-27 23:29:06 -0400

    trying to create second table

--- mysql_controller.py
+++ mysql_controller.py
@@ -20,7 +20,12 @@ with open("data.json") as json_data_file:
 #     cursor.execute("INSERT INTO recipes(title, summary, rating, rating_count, url, author) VALUES (%s, %s, %s, %s, %s, %s)", 
 #     (items['title'], items['summary'], items['rating'], items['rating_count'], items['url'], items['author']))
 
-cursor.execute('SELECT * FROM recipes')
+# cursor.execute('SELECT id, title, rating FROM recipes WHERE rating > 4.8')
 
-for x in cursor:
-    print(x)
+# for x in cursor:
+#     print(x)
+
+cursor.execute('CREATE TABLE recipe_details (recipe_id int PRIMARY KEY, FOREIGN KEY(recipe_id) REFERENCES recipes(id), prep VARCHAR(20), cook VARCHAR(20), add VARCHAR(20), total VARCHAR(20), servings INT, yield VARCHAR(20), ingredients TEXT, directions TEXT)')
+db.commit()
+# for items in data:
+#     print(items['metadata']['additional'])
